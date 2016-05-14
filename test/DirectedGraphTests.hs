d3378a6d229971329d441e774aba42b808210ce4
William Brandon <hypercube97@gmail.com>
2016-05-14 16:24:02 -0700

    Add unit tests for DirectedGraph

--- test/DirectedGraphTests.hs
+++ test/DirectedGraphTests.hs
@@ -4,6 +4,47 @@ import DirectedGraph
 
 import Test.Hspec
 
+import qualified Data.Map as Map
+import Data.Map (Map)
+import qualified Data.Set as Set
+
+edgesEqualUpToPermutation :: (Show a, Ord a) => Map a [a] -> Map a [a] -> Expectation
+edgesEqualUpToPermutation xs ys =
+  (Map.map Set.fromList xs) `shouldBe` (Map.map Set.fromList ys)
+
 test :: Spec
 test = describe "DirectedGraph" $ do
-  return ()
+  let
+    graph = buildDirectedGraph
+      [ 'A' `EdgeTo` 'B'
+      , 'B' `EdgeTo` 'C'
+      , 'D' `EdgeTo` 'E'
+      , 'D' `EdgeTo` 'G'
+      , 'F' `EdgeTo` 'E'
+      , 'F' `EdgeTo` 'G'
+      , 'E' `EdgeTo` 'E'
+      , 'G' `EdgeTo` 'F'
+      ]
+
+  let
+    expectedIncoming = Map.fromList
+      [ ('B', ['A'])
+      , ('C', ['B'])
+      , ('E', ['D', 'F', 'E'])
+      , ('G', ['F', 'D'])
+      , ('F', ['G'])
+      ]
+
+  let
+    expectedOutgoing = Map.fromList
+      [ ('A', ['B'])
+      , ('B', ['C'])
+      , ('D', ['E', 'G'])
+      , ('F', ['E', 'G'])
+      , ('E', ['E'])
+      , ('G', ['F'])
+      ]
+
+  it "finds incoming edges" $ edgesEqualUpToPermutation expectedIncoming (incomingEdges graph)
+
+  it "finds outgoing edges" $ edgesEqualUpToPermutation expectedOutgoing (outgoingEdges graph)
