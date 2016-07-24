23759809c03fbf7388586713c5a9271234c61629
William Brandon <hypercube97@gmail.com>
2016-07-23 21:06:01 -0700

    Add automatic handling of var aliasing
    Previously, special handling was required when a var occurred multiple
    times in a single constraint, because treating those occurrences of the
    var as independent would result in it being written to twice, with the
    second value overwriting the first, thereby breaking constraint
    propagation.  Now, instead of handling that scenario in the constraint
    enforcer code, which was a fragile and ugly hack, it’s handled
    automatically in the general-purpose propagation logic.  While this is
    probably slightly less performant because it involves checking for var
    aliasing on every var every time a constraint is applied, it makes for
    much cleaner and more robust enforcer code.
    
    Additionally, in implementing this change, I encountered and fixed a
    silly bug where “last seen versions” were never updated, and therefore
    every variable was considered Changed every time a constraint was
    applied.  Since the Changed/Unchanged distinction is just a performance
    optimization, this bug didn’t show up until now because it manifested
    only in slightly less efficient constraint propagation.

--- lib/Propagate.hs
+++ lib/Propagate.hs
@@ -14,6 +14,7 @@ module Propagate
 where
 
 import qualified Data.Map as Map
+import Data.Map (Map)
 
 import qualified Data.Set as Set
 import Data.Set (Set)
@@ -23,8 +24,9 @@ import Data.STRef hiding (modifySTRef)
 
 import Control.Monad.Trans.Except
 import Control.Monad.Trans (lift)
-import Control.Monad (forM, forM_)
+import Control.Monad (forM, forM_, join)
 
+import Data.Bifunctor (second)
 import Data.Maybe (fromMaybe, fromJust)
 
 import FoldInsert (fromListWithCons)
@@ -42,6 +44,7 @@ data Problem var val err = Problem
   { problemInitialVals :: [(var, val)]
   , problemDefaultVal :: val
   , problemConstraints :: [ConstraintEnforcer var val err]
+  , problemMergeUpdates :: var -> val -> val -> Either err val
   }
 
 queryVar :: var -> ConstraintQuery var val (val, ChangeStatus)
@@ -93,8 +96,31 @@ liftEither :: (Monad m) => Either e a -> ExceptT e m a
 liftEither (Left e) = throwE e
 liftEither (Right x) = return x
 
+data MergeStatus = DidNotMerge | DidMerge
+
+data Update val = Update
+  { mergeStatus :: MergeStatus
+  , newValue :: val
+  }
+
+mapFromListWithKeyM :: (Ord k, Monad m) => (k -> a -> a -> m a) -> [(k, a)] -> m (Map k a)
+mapFromListWithKeyM f =
+  mapM id .
+  Map.fromListWithKey (\k a b -> join (f k <$> a <*> b)) .
+  map (second return)
+
 solve :: forall var val err. (Ord var, Eq val) => Problem var val err -> Either err (var -> Maybe val)
 solve problem = runST $ runExceptT go where
+  mergeUpdates :: var -> Update val -> Update val -> Either err (Update val)
+  mergeUpdates var (Update _ val1) (Update _ val2) =
+    fmap (Update DidMerge) $ problemMergeUpdates problem var val1 val2
+
+  mergeAllUpdates :: [(var, val)] -> Either err [(var, Update val)]
+  mergeAllUpdates =
+    fmap Map.toList .
+    mapFromListWithKeyM mergeUpdates .
+    map (second (Update DidNotMerge))
+
   go :: forall s. Op s err (var -> Maybe val)
   go = do
     let indexedConstraints = zip [(ConstraintID 0)..] (problemConstraints problem)
@@ -156,14 +182,35 @@ solve problem = runST $ runExceptT go where
                   ,"last seen version is later than most recent version"]
             return (dep, (currentVal, status))
         updates <- liftEither $ queryCompute (constraintEnforcer constraint) lookupForQuery
-        forM_ updates $ \(var, newVal) -> do
+        mergedUpdates <- liftEither $ mergeAllUpdates updates
+        forM_ mergedUpdates $ \(var, update) -> do
           let node = lookupNode var
           oldVal <- lift $ readSTRef $ nodeVal node
+          let newVal = newValue update
           if oldVal /= newVal
           then do
-            lift $ modifySTRef' (nodeVersion node) succ
+            newVersion <- fmap succ $ lift $ readSTRef $ nodeVersion node
+            lift $ writeSTRef (nodeVersion node) newVersion
+            case mergeStatus update of
+              DidNotMerge ->
+                lift $ writeSTRef (constraintLastSeenVersions constraint var) newVersion
+              DidMerge ->
+                {- Since a merge operation was applied, this var may not have any of the values
+                that the constraint wrote to it.  This means we should consider it changed from
+                the perspective of the constraint, so we should *not* update the constraint's
+                last seen version to match the new version.
+                -}
+                return ()
             lift $ writeSTRef (nodeVal node) newVal
-            let newDirtyConstraints = Set.delete constraintID $ nodeConstraints node
+            let
+              newDirtyConstraints =
+                case mergeStatus update of
+                  DidNotMerge -> Set.delete constraintID $ nodeConstraints node
+                  DidMerge ->
+                    {- Since the last seen version is not the current version, the constraint
+                    currently being applied is still dirty.
+                    -}
+                    nodeConstraints node
             lift $ modifySTRef' dirtyConstraints (`Set.union` newDirtyConstraints)
           else
             return ()
